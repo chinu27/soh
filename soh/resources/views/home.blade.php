86e31830942ef5a4a9f5ad03ded5d835da3527d4
chinu27 <infy.chinmaysharma@gmail.com>
2018-08-23 01:04:46 +0530

    version 1.0

--- soh/resources/views/home.blade.php
+++ soh/resources/views/home.blade.php
@@ -6,18 +6,16 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Chats</div>
+                <div class="panel-heading">Live Chat BoX</div>
 
                 <div class="panel-body">
-                    <chat-messages :messages="messages"></chat-messages>
-                    ye kya hai..
-                </div>
-                <div class="panel">
+                    <chat-messages :messages="messages"></chat-messages> </div>
+                <div class="panel-footer">
                     <chat-form
                         v-on:messagesent="addMessage"
                         :user="{{ Auth::user() }}"
                     ></chat-form>
-                </div> 
+                </div>
             </div>
         </div>
     </div>
